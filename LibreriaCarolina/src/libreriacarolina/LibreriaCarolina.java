73e05518e44d4a33fbb2389c6b30958919a27f53
linke <linke@DESKTOP-GAG07PL>
2020-04-15 19:28:54 -0500

    Conexion a la base de datos

---
+++ LibreriaCarolina/src/libreriacarolina/LibreriaCarolina.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreriacarolina;
+
+/**
+ *
+ * @author linke
+ */
+public class LibreriaCarolina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Conexion BD = new Conexion ();
+      BD.conexion();
+    }
+    
+}
