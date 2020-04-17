455b450094558df98e2ea803302840399f616a89
linke <linke@DESKTOP-GAG07PL>
2020-04-16 21:02:43 -0500

    Formulario menu para vendedores

--- LibreriaCarolina/src/formularios/frmMenuVendedor.java
+++ LibreriaCarolina/src/formularios/frmMenuVendedor.java
@@ -9,13 +9,17 @@ package formularios;
  *
  * @author linke
  */
-public class frmMenuAdmin extends javax.swing.JFrame {
+public class frmMenuVendedor extends javax.swing.JFrame {
 
     /**
      * Creates new form frmMenuAdmin
      */
-    public frmMenuAdmin() {
+    public frmMenuVendedor() {
         initComponents();
+        txtAreaAdministrador.setEnabled(false);
+        txtProveedor.setEnabled(false);
+        txtPedido.setEnabled(false);
+        txtEmpleados.setEnabled(false);
     }
 
     /**
@@ -229,20 +233,21 @@ public class frmMenuAdmin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmMenuAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmMenuVendedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmMenuAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmMenuVendedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmMenuAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmMenuVendedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmMenuAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmMenuVendedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmMenuAdmin().setVisible(true);
+                new frmMenuVendedor().setVisible(true);
             }
         });
     }
